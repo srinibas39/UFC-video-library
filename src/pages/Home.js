dceb378472856a8004ad4403d4389cc77773c8be
srinibas <srinibaskhuntia39@gmail.com>
2022-05-31 08:42:36 +0530

    fixed homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,10 @@
 import { Hero } from "../components/Hero/Hero"
 import { NavBar } from "../components/Navbar/Navbar"
 
-export const Home=()=>{
-    return <>
-       <NavBar/>
-       <Hero/>
-       
-    </>
+export const Home = () => {
+    return <div style={{ height: "88vh" }}>
+        <NavBar />
+        <Hero />
+
+    </div>
 }
