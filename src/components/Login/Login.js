9e4c25651df8c051c2903d6966ad7c06df067e71
srinibas <srinibaskhuntia39@gmail.com>
2022-07-25 15:45:17 +0530

    implemented toast for login and signup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import "./Login.css"
 import { useAuth } from "../../context/AuthContext";
 import { useEffect } from "react";
+import { handleToast, handleToastWarning } from "../../utils/toastUtils";
+import { ToastContainer } from 'react-toastify';
 
 export const Login = () => {
     const navigate = useNavigate();
@@ -17,47 +19,56 @@ export const Login = () => {
     }, [token])
 
     const handleLogin = () => {
-        if(form.email.trim().length && form.password.trim().length){
-            loginHandler(form.email, form.password);
-            navigate("/explore")
+        if (form.email.trim().length && form.password.trim().length) {
+            handleToast("login successful")
+            setTimeout(() => {
+                loginHandler(form.email, form.password);
+                navigate("/explore");
+            }, 1500)
 
         }
-        else{
-            console.log("plase fill up the login form");
+        else {
+            handleToastWarning("Please fill up the login form");
         }
     }
 
     const handleGuestLogin = () => {
-        loginHandler("srinibaskhuntia39@gmail.com", "srinibaskhuntia");
-        navigate("/explore")
+        handleToast("login successful")
+        setTimeout(() => {
+            loginHandler("srinibaskhuntia39@gmail.com", "srinibaskhuntia");
+            navigate("/explore");
+        }, 1500)
 
     }
 
-    return <div className="login-container">
-        <div className="login">
-            <h2>Login</h2>
-            <div className="email-container">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} />
-            </div>
-            <div className="password-container">
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} />
-            </div>
-            <div className="forgotP-container">
-                <div className="remember">
-                    <input type="checkbox" name="remember-me" />
-                    <label htmlFor="remember-me">Remember Me</label>
+    return <>
+        <div className="login-container">
+            <div className="login">
+                <h2>Login</h2>
+                <div className="email-container">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} />
                 </div>
-                <a href="#">Forgot Your Password ?</a>
-            </div>
-            <button className="btn-logins" onClick={handleLogin}>Login</button>
-            <button className="btn-logins" onClick={handleGuestLogin}>Guest Login</button>
+                <div className="password-container">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} />
+                </div>
+                <div className="forgotP-container">
+                    <div className="remember">
+                        <input type="checkbox" name="remember-me" />
+                        <label htmlFor="remember-me">Remember Me</label>
+                    </div>
+                    <a href="#">Forgot Your Password ?</a>
+                </div>
+                <button className="btn-logins" onClick={handleLogin}>Login</button>
+                <button className="btn-logins" onClick={handleGuestLogin}>Guest Login</button>
 
-            <div className="create-new" onClick={() => navigate("/signup")}>
-                <p>Create new Account</p>
-                <span className="material-icons"> arrow_forward_ios </span>
+                <div className="create-new" onClick={() => navigate("/signup")}>
+                    <p>Create new Account</p>
+                    <span className="material-icons"> arrow_forward_ios </span>
+                </div>
             </div>
         </div>
-    </div>
+        <ToastContainer />
+    </>
 }
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom"
 import { useState } from "react";
 import "./Signup.css"
 import { useAuth } from "../../context/AuthContext";
+import { ToastContainer } from 'react-toastify';
+import { handleToast, handleToastWarning } from "../../utils/toastUtils";
 
 export const Signup = () => {
     const navigate = useNavigate();
@@ -11,44 +13,50 @@ export const Signup = () => {
 
     const handleSignup = () => {
         if (form.email.trim().length && form.password.trim().length && form.firstName.trim().length && form.lastName.trim().length) {
-            signupHandler(form.email, form.password, form.firstName, form.lastName);
-            navigate("/explore")
+            handleToast("Signup successful");
+            setTimeout(() => {
+                signupHandler(form.email, form.password, form.firstName, form.lastName);
+                navigate("/explore")
+            }, 1500)
         }
-        else{
-            console.log("plase fill up the sign up form");
+        else {
+            handleToastWarning("Please fill up the sign up form");
         }
     }
 
-    return <div className="signup-container">
-        <div className="signup">
-            <h2>Sign up</h2>
-            <div className="firstname-container">
-                <label htmlFor="firstname">Firstname</label>
-                <input type="firstname" name="firstname" value={form.firstName} onChange={(e) => setForm({ ...form, firstName: e.target.value })} />
-            </div>
-            <div className="lastname-container">
-                <label htmlFor="lastname">Lastname</label>
-                <input type="lastname" name="lastname" value={form.lastName} onChange={(e) => setForm({ ...form, lastName: e.target.value })} />
-            </div>
-            <div className="email-container">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} />
-            </div>
-            <div className="password-container">
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} />
-            </div>
+    return <>
+        <div className="signup-container">
+            <div className="signup">
+                <h2>Sign up</h2>
+                <div className="firstname-container">
+                    <label htmlFor="firstname">Firstname</label>
+                    <input type="firstname" name="firstname" value={form.firstName} onChange={(e) => setForm({ ...form, firstName: e.target.value })} />
+                </div>
+                <div className="lastname-container">
+                    <label htmlFor="lastname">Lastname</label>
+                    <input type="lastname" name="lastname" value={form.lastName} onChange={(e) => setForm({ ...form, lastName: e.target.value })} />
+                </div>
+                <div className="email-container">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} />
+                </div>
+                <div className="password-container">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} />
+                </div>
 
-            <div className="terms">
-                <input type="checkbox" name="remember-me" />
-                <label htmlFor="terms">{`I accept all terms & conditions`}</label>
-            </div>
+                <div className="terms">
+                    <input type="checkbox" name="remember-me" />
+                    <label htmlFor="terms">{`I accept all terms & conditions`}</label>
+                </div>
 
-            <button className="btn-register" onClick={handleSignup}>Create New Account</button>
-            <div className="account-login" onClick={() => navigate("/login")}>
-                <p>Already have an account</p>
-                <span className="material-icons"> arrow_forward_ios </span>
+                <button className="btn-register" onClick={handleSignup}>Create New Account</button>
+                <div className="account-login" onClick={() => navigate("/login")}>
+                    <p>Already have an account</p>
+                    <span className="material-icons"> arrow_forward_ios </span>
+                </div>
             </div>
         </div>
-    </div>
+        <ToastContainer/>
+    </>
 }
