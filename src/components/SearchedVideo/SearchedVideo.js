0c71c4601813faac9c940b17c23f4b2fddfbbdb7
srinibas <srinibaskhuntia39@gmail.com>
2022-09-17 19:55:11 +0530

    removed issues

--- src/components/SearchedVideo/SearchedVideo.js
+++ src/components/SearchedVideo/SearchedVideo.js
@@ -23,7 +23,7 @@ export const SearchedVideo = () => {
                 <Categories />
                 <div className="likes">
                     <div className="searched-header" id={mode ? "dark-font" : ""}>
-                        <h2 style={{ textAlign: "center" }}>{`Search Results for "${text}"`}</h2>
+                        {/* <h2 style={{ textAlign: "center" }}>{`Search Results for "${text}"`}</h2> */}
                     </div>
                     <div className="card-container">
                         {
