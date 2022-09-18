40557002ecb05e1256a1fe68eb0dc6259b3803db
srinibas <srinibaskhuntia39@gmail.com>
2022-09-18 13:25:35 +0530

    bugs removed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import { useAuth } from "../../context/AuthContext";
 import { useMode } from "../../context/ModeContext";
 import { handleToast } from "../../utils/toastUtils";
@@ -20,23 +19,23 @@ export const Profile = () => {
 
   return (
     <>
-      {/* <div className="profile" id={mode ? "dark" : ""}>
+      <div className="profile" id={mode ? "dark" : ""}>
         <div className="profile-header">
           <h2>PROFILE</h2>
         </div>
         <div className="profile-body">
           <p>
             <span className="material-symbols-outlined">account_circle</span>
-            {user ? user.firstName + " " + user.lastName : ""}
+            {user?.firstName + " " + user?.lastName}
           </p>
           <p>
             <span className="material-symbols-outlined">mail</span>
-             {user ? user.firstName + " " + user.lastName : ""}
+            {user?.email}
           </p>
           <button onClick={handleLogout}>Logout</button>
         </div>
       </div>
-      <ToastContainer /> */}
+      
     </>
   );
 };
